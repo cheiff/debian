637ee19f5e668b70ae38bf8cafe199c57071ce67
Javi en Nostromo <javier@octech.es>
2013-10-28 14:57:29 +0100

    install basics

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /root/
 dpkg-reconfigure locales
 dpkg-reconfigure tzdata
 apt-get update
@@ -8,6 +9,7 @@ hostname
 git clone git://github.com/cheiff/rc
 rc/install.sh
 ssh-keygen -t dsa
+ssh-add
 echo "Add this key to gitosis"
 cat .ssh/id_dsa.pub
 
