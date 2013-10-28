04b51953d60637c2d0c96cd21907d85f7293debc
Javi en Nostromo <javier@octech.es>
2013-10-28 15:16:19 +0100

    ~

--- install.sh
+++ install.sh
@@ -8,9 +8,16 @@ hostname $(hostname -f)
 hostname
 git clone git://github.com/cheiff/rc
 rc/install.sh
+mkdir -p .ssh
+chmod 700 .ssh
+cat >.ssh/config<<EOF
+ControlMaster auto
+ControlPath /tmp/ssh_mux_%h_%p_%r
+EOF
 ssh-keygen -t dsa
 eval `ssh-agent` && ssh-add ~/.ssh/id_dsa
 echo "Add this key to gitosis"
 cat .ssh/id_dsa.pub
-
+read CONTINUE
+tmux
 
