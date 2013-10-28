4c63ee76feab9af6382711eb30515ff3a44a4069
Javi en Nostromo <javier@octech.es>
2013-10-28 15:10:43 +0100

    ~ip.sh

--- ip.sh
+++ ip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IP=$(ifconfig  | grep -i 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
+IP=$(/sbin/ifconfig  | grep -i 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
 echo $IP
