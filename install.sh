c354f52618dbdfb3d323d498759a4bc09ab5b051
Javi en Nostromo <javier@octech.es>
2013-10-28 14:55:18 +0100

    install basics

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ apt-get update
 hostname
 hostname $(hostname -f)
 hostname
+git clone git://github.com/cheiff/rc
+rc/install.sh
 ssh-keygen -t dsa
 echo "Add this key to gitosis"
 cat .ssh/id_dsa.pub
