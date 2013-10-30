c9580793a150e181ab626588038dcda21214b575
sphinx <sphinx@kronos>
2013-10-30 12:38:09 +0100

    ~install

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-if [ $# != 5 ];then
+if [ $# != 4 ];then
   echo "Syntax: install name repo saltmasterIP hostname"
   exit
 fi
 cd /root/
 dpkg-reconfigure tzdata
 apt-get update
-hostname ${6}
+hostname ${4}
 git clone git://github.com/cheiff/rc
 rc/install.sh
 git clone git://github.com/cheiff/scripts
 echo "PATH=\$PATH:/root/scripts/run">>.bashrc
-echo "$4 saltmaster">>/etc/hosts
+echo "$3 saltmaster">>/etc/hosts
 mkdir -p .ssh
 chmod 700 .ssh
 cat >.ssh/config<<EOF
@@ -19,7 +19,7 @@ ControlMaster auto
 ControlPath /tmp/ssh_mux_%h_%p_%r
 EOF
 git config --global user.name "$1"
-git config --global user.email "root@$6"
+git config --global user.email "root@$4"
 
 
 ssh-keygen -t dsa
@@ -27,7 +27,7 @@ eval `ssh-agent` && ssh-add ~/.ssh/id_dsa
 echo "Add this key to gitosis"
 cat .ssh/id_dsa.pub
 read CONTINUE
-git clone $3 -b debian
+git clone $2 -b debian
 tmux
 
 
